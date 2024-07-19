5491ec02ef5ab88ca98b2b4adeb5859a479de63d
nvsvtrp <kiryl.kushnerau@innowise.com>
2024-07-19 14:22:38 +0300

    Tets

--- tests/elementsTest.spec.ts
+++ tests/elementsTest.spec.ts
@@ -36,8 +36,6 @@ test ('Click to TextBox button. Enter incorrect data in email field. The entered
     await expect (pageManager.elements.TextBoxLabels.emailLabel).toBeHidden();
     await expect (pageManager.elements.TextBoxLabels.currentAdressLabel).toBeHidden();
     await expect (pageManager.elements.TextBoxLabels.permanentAdressLabel).toBeHidden();
-
-
 })
 
 
