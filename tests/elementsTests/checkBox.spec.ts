084d7c9aae5b2ada744209b2c52ce5b19d5a9298
nvsvtrp <kiryl.kushnerau@innowise.com>
2024-07-22 10:49:26 +0300

    New commit

---
+++ tests/elementsTests/checkBox.spec.ts
@@ -0,0 +1,35 @@
+import { PageManager } from "../../Page Object/pageManager";
+import { test } from "../baseTest";
+import { expect } from "@playwright/test";
+
+test.describe('Open the main page of the site. Click to the Elements button. The elements page has opened.', () => {
+    test.beforeEach (async ({pageManager})=> {
+    await pageManager.main.Buttons.elementsButton.click();
+    }) 
+
+
+    test ('Click to CheckBox button. Click to the plus button. List is opened. Click to the minus button. List is closed.', async ({page, pageManager}) => {
+        await pageManager.checkBox.Buttons.checkBoxButton.click();
+        await pageManager.checkBox.Buttons.expandButton.click();
+        await expect (page.getByText('Angular')).toBeVisible();
+        await pageManager.checkBox.Buttons.checkBoxButton.click();
+        await pageManager.checkBox.Buttons.collapseButton.click();
+        await expect (page.getByText('Angular')).toHaveCount(0);
+    })
+
+    test ('Click to CheckBox button. Click to the Home checkbox. Result label show all branch.', async ({page, pageManager}) => {
+        await pageManager.checkBox.Buttons.checkBoxButton.click();
+        await pageManager.checkBox.Buttons.homeCheckbox.click();
+        await expect (pageManager.checkBox.Buttons.homeCheckbox).toBeChecked();
+        await expect (pageManager.checkBox.Labels.resultLabel).toContainText('excelFile ');
+    
+    })
+
+    test ('Click to CheckBox button. Click to the Home checkBox. Click to the downloads checkbox. Resulat label show downloads, wordFile and excelFile.', async ({page, pageManager}) => {
+        await pageManager.checkBox.Buttons.checkBoxButton.click();
+        await pageManager.checkBox.Buttons.arrowHomeButton.click();
+        await page.getByText('Downloads').click();
+        await expect (pageManager.checkBox.Labels.resultLabel).toContainText('excelFile ');
+    })
+})
+
