0d3d9321b2acd2ddf637f207562cacf662ac1912
nvsvtrp <kiryl.kushnerau@innowise.com>
2024-07-26 11:57:23 +0300

    add all

--- Page Object/Widgets/datePickerPage.ts
+++ Page Object/Widgets/datePickerPage.ts
@@ -3,4 +3,16 @@ export class DatePickerPage extends basePage {
     constructor (page) {
         super (page)
     }
+
+    Buttons = {
+        datePickerButton: this.page.locator("li").filter({ hasText: "Date Picker" }),
+        selectDateButton: this.page.locator('#datePickerMonthYearInput'),
+        selectDateAndTimeButton: this.page.locator('#dateAndTimePickerInput'),
+        monthSelectButton: this.page.locator('.react-datepicker__month-select'),
+        yearSelectButton: this.page.locator('.react-datepicker__year-select'),
+    }
+
+    Labels = {
+        selectedDayLabel: this.page.locator('.react-datepicker__day--selected')
+    }
 }
