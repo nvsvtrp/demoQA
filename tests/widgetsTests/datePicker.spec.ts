5191d345b17a82410a32d6829cf9e78f78b7126b
nvsvtrp <kiryl.kushnerau@innowise.com>
2024-07-26 13:03:59 +0300

    Date Picker Tests

--- tests/widgetsTests/datePicker.spec.ts
+++ tests/widgetsTests/datePicker.spec.ts
@@ -6,17 +6,17 @@ test.describe("Go to the Date Picker page by url. The Date Picker page has opene
     await page.goto("https://demoqa.com/date-picker");
   });
 
-  test(' "Select Date" label shows todays date', async ({ pageManager }) => {
+  test(' "Select Date" label shows current date', async ({ pageManager }) => {
     const todayDate = getTodayDate();
     await expect(pageManager.datePicker.Buttons.selectDateButton).toHaveValue(todayDate);
   });
 
-  test("Click to the Dynamic Properties Buter.", async ({ pageManager }) => {
+  test("'Select Date and Time' label shows current date and time ", async ({ pageManager }) => {
     const currentDateTime = getCurrentDateTime();
     await expect(pageManager.datePicker.Buttons.selectDateAndTimeButton).toHaveValue(currentDateTime);
   });
 
-  test("Click to the.", async ({ pageManager }) => {
+  test("Click to the 'Select Date'. The opend window displays current date which is highlighted in color.", async ({ pageManager }) => {
     await pageManager.datePicker.Buttons.selectDateButton.click();
     await expect(pageManager.datePicker.Labels.selectedDayLabel).toContainText(getTodayDayNumber());
     await expect(pageManager.datePicker.Labels.selectedDayLabel).toHaveCSS(
@@ -25,13 +25,24 @@ test.describe("Go to the Date Picker page by url. The Date Picker page has opene
     )
   })
 
-  test("Click to thed.", async ({ pageManager }) => {
+  test("Click to the 'Select Date'. Select a specific month and day. Selected values shows in Select Date Label.", async ({ page, pageManager }) => {
     await pageManager.datePicker.Buttons.selectDateButton.click();
-    await expect(pageManager.datePicker.Labels.selectedDayLabel).toContainText(getTodayDayNumber());
-    await expect(pageManager.datePicker.Labels.selectedDayLabel).toHaveCSS(
-      "background-color",
-      "rgb(33, 107, 165)",
-    )
+    await pageManager.datePicker.Buttons.monthSelectButton.selectOption('2');
+    await pageManager.datePicker.Buttons.yearSelectButton.selectOption('1998');
+    await page.getByLabel('Choose Sunday, March 29th,').click();
+    await expect(pageManager.datePicker.Buttons.selectDateButton).toHaveValue('03/29/1998');
+  })
+
+  test("Click to the 'Select Date and Time'. Select a specific month,day. Selected values shows in Select Date Label.", async ({ page, pageManager }) => {
+    await pageManager.datePicker.Buttons.selectDateAndTimeButton.click();
+    await page.locator('.react-datepicker__month-read-view--down-arrow').click();
+    await page.getByText('March').click();
+    await page.getByText('2024', { exact: true }).click();
+    await page.locator('a').nth(3).click({clickCount: 25});
+    await page.getByText('1998').click();
+    await page.getByText('15:15').click();
+    await expect(pageManager.datePicker.Buttons.selectDateAndTimeButton).toHaveValue('July 26, 2024 3:15 PM');
+
   })
 
   function getTodayDate(): string {
