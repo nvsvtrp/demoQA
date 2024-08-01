27c1d1768dcc4a43ab90559b1242c21bbd1ac3a0
nvsvtrp <kiryl.kushnerau@innowise.com>
2024-08-01 10:56:52 +0300

    Commit

--- tests/bookTests/login.spec.ts
+++ tests/bookTests/login.spec.ts
@@ -1,41 +1,43 @@
-import { test } from "../baseTest";
-import { expect } from "@playwright/test";
+// import { test } from "../baseTest";
+// import { expect } from "@playwright/test";
 
-test.describe("Open login page", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/login");
-  });
+// test.describe("Open login page", () => {
+//   test.beforeEach(async ({ page }) => {
+//     await page.goto("/login");
+//   });
 
-  test('Register user via API.', async ({ pageManager, pageManagerAPI, page }) => {
-    const userName = pageManager.randomValues.getRandomUserName();
-    const password = pageManager.randomValues.getRandomPassword();
-    console.log(userName);
-    console.log(password);
-    await registerUser(userName, password, pageManager, pageManagerAPI, page);
-    await pageManager.login.Fields.userNameField.fill(userName);
-    await pageManager.login.Fields.passwordField.fill(password);
-    await pageManager.login.Buttons.loginButton.click();
-    await expect(pageManager.login.Labels.userNameLabel).toContainText(userName);
-  });
+//   test('Register user via API.', async ({ pageManager, pageManagerAPI, page }) => {
+//     const userName = pageManager.randomValues.getRandomUserName();
+//     const password = pageManager.randomValues.getRandomPassword();
+//     console.log(userName);
+//     console.log(password);
+//     await registerUser(userName, password, pageManager, pageManagerAPI, page);
+//     await pageManager.login.Fields.userNameField.fill(userName);
+//     await pageManager.login.Fields.passwordField.fill(password);
+//     await pageManager.login.Buttons.loginButton.click();
+//     await expect(pageManager.login.Labels.userNameLabel).toContainText(userName);
+//   });
 
-  test('Add book via API.', async ({ pageManager, pageManagerAPI, page }) => {
-    const userName = pageManager.randomValues.getRandomUserName();
-    const password = pageManager.randomValues.getRandomPassword();
-    await registerUser(userName, password, pageManager, pageManagerAPI, page);
-    const isbn = await pageManagerAPI.loginAPI.getRandomBooksId();
-    const token = await pageManagerAPI.loginAPI.generateToken(userName, password);
-    const userId = await pageManagerAPI.loginAPI.login(userName, password);
-    await pageManagerAPI.loginAPI.postBookToAccount(userId, isbn, token);
-    await pageManager.login.Fields.userNameField.fill(userName);
-    await pageManager.login.Fields.passwordField.fill(password);
-    await pageManager.login.Buttons.loginButton.click();
-    await expect(pageManager.login.Labels.userNameLabel).toContainText(userName);
-  });
+//   test('Add book via API.', async ({ pageManager, pageManagerAPI, page }) => {
+//     const userName = pageManager.randomValues.getRandomUserName();
+//     const password = pageManager.randomValues.getRandomPassword();
+//     console.log(userName)
+//     console.log(password)
+//     await registerUser(userName, password, pageManager, pageManagerAPI, page);
+//     const isbn = await pageManagerAPI.loginAPI.getRandomBooksId();
+//     const token = await pageManagerAPI.loginAPI.generateToken(userName, password);
+//     const userId = await pageManagerAPI.loginAPI.login(userName, password);
+//     await pageManagerAPI.loginAPI.postBookToAccount(userId, isbn, token);
+//     await pageManager.login.Fields.userNameField.fill(userName);
+//     await pageManager.login.Fields.passwordField.fill(password);
+//     await pageManager.login.Buttons.loginButton.click();
+//     await expect(pageManager.login.Labels.userNameLabel).toContainText(userName);
+//   });
 
-  async function registerUser(userName: string, password: string, pageManager, pageManagerAPI, page) {
-    await pageManager.login.Buttons.newUserButton.click();
-    await pageManagerAPI.loginAPI.registerUserRequest(userName, password);
-    await pageManager.login.Buttons.backToLoginButton.click();
-  }
+//   async function registerUser(userName: string, password: string, pageManager, pageManagerAPI, page) {
+//     await pageManager.login.Buttons.newUserButton.click();
+//     await pageManagerAPI.loginAPI.registerUserRequest(userName, password);
+//     await pageManager.login.Buttons.backToLoginButton.click();
+//   }
 
-});
+// });
