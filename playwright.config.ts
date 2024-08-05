d68f6a00df5bec398a8861366909242d519fd9e6
nvsvtrp <kiryl.kushnerau@innowise.com>
2024-08-05 20:27:28 +0300

    Allure reporter First

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,32 +1,38 @@
 import { defineConfig, devices } from "@playwright/test";
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+import { testPlanFilter } from "allure-playwright/testplan";
+
 export default defineConfig({
+  grep: testPlanFilter(),
   testDir: "./tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [
+    ["line"],
+    [
+      "allure-playwright",
+      {
+        detail: true,
+        outputFolder: "allure-results",
+        suiteTitle: true,
+        categories: [
+          {
+            name: "Outdated tests",
+            messageRegex: ".*FileNotFound.*",
+          },
+        ],
+        environmentInfo: {
+          framework: "playwright",
+        },
+      },
+    ],
+  ],
   use: {
     trace: "on",
     baseURL: "https://demoqa.com",
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
